ab0899022b7491bdfa5c1cc72005690b2ffb5098
Asylkhan <assylkhan.rakhatov@mail.ru>
2014-09-30 02:20:50 +0600

    wicket_pdf instead of prawn

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,11 +1,6 @@
-if Rails.env.production?
-  WickedPdf.config = {
-    exe_path: "/opt/application/current/gems/wkhtmltopdf-binary-#{Gem.loaded_specs['wkhtmltopdf-binary'].version}/bin/wkhtmltopdf_linux_x64"
-  }
-else
-  WickedPdf.config = {
-    #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
-    #:layout => "pdf.html",
-    :exe_path => '/usr/local/bin/wkhtmltopdf'
-  }
-end
+# WickedPdf.config = {
+#   wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-amd64"
+#   #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
+#   #:layout => "pdf.html",
+#   # :exe_path => '/usr/local/bin/wkhtmltopdf'
+# }
