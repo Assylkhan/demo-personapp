6412739fd1270a8783b83208e982aabd984b78c0
Asylkhan <assylkhan.rakhatov@mail.ru>
2014-09-30 01:10:51 +0600

    wicket_pdf instead of prawn

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -3,3 +3,11 @@ WickedPdf.config = {
   #:layout => "pdf.html",
   :exe_path => '/usr/local/bin/wkhtmltopdf'
 }
+
+module WickedPdfHelper
+  def wicked_pdf_stylesheet_link_tag(*sources)
+  sources.collect { |source|
+  "<style type='text/css'>#{Rails.application.assets.find_asset("#{source}.css")}</style>"
+  }.join("\n").gsub(/url\(['"](.+)['"]\)(.+)/,%[url("#{wicked_pdf_image_location("\\1")}")\\2]).html_safe
+  end
+end
